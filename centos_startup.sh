a622ee9809b114aa6acc0499cd71f02b55e8a8b9
skydve <adrobinko@gmail.com>
2018-01-10 01:03:47 +0300

    fixed errors

--- centos_startup.sh
+++ centos_startup.sh
@@ -3,6 +3,7 @@
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
-sudo usermod -a -G docker ec2-usermod
+sudo usermod -a -G docker ec2-user
+echo "done!"
 
 #logout and login
